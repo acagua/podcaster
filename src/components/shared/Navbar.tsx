44d486e935b4266d147ea02c60f99746c4327118
Andres Cagua <af.cagua1011@gmail.com>
2023-06-13 15:57:56 -0500

    setup context base

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { useContext } from "react";
+import { PodcasterContext } from "../../layouts/AppLayout";
 export default function Navbar() {
+  const context = useContext(PodcasterContext);
   return (
     <nav className={styles.nav}>
       <Link to="/" className={styles.link}>
         Podcaster
       </Link>
-      <div className={styles.loader} />
+      {context?.loading && <div className={styles.loader} />}
     </nav>
   );
 }
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,15 +1,32 @@
 import Navbar from "../components/shared/Navbar";
 import styles from "./AppLayout.module.css";
 import { Outlet } from "react-router-dom";
+import { useState, createContext, Dispatch, SetStateAction } from "react";
+
+interface PodcasterContextInterface {
+  loading: boolean;
+  setLoading: Dispatch<SetStateAction<boolean>>;
+  podcastList: string[];
+  setPodcastList: Dispatch<SetStateAction<string[]>>;
+}
+export const PodcasterContext = createContext<PodcasterContextInterface | null>(
+  null
+);
+
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [podcastList, setPodcastList] = useState<string[]>([]);
+
   return (
-    <>
+    <PodcasterContext.Provider
+      value={{ loading, setLoading, podcastList, setPodcastList }}
+    >
       <Navbar />
       <h1 className={styles.title}>Podcaster App</h1>
       <div>Content</div>
       <main>
         <Outlet />
       </main>
-    </>
+    </PodcasterContext.Provider>
   );
 }
